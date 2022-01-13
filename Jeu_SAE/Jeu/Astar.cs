f7cf9c8026db1beabd4f2b563ef7bf69bcd20c93
DESKTOP-0DRPE70\remy <Remy.Guyon@etu.univ-savoie.fr>
2022-01-14 00:44:16 +0100

    Création astar (non-intégré au code)

---
+++ Jeu_SAE/Jeu/Astar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading;
+
+namespace Jeu
+{
+    class Astar
+    {
+        public static void AlgoAStar()
+        {
+            List<Node> Open = new List<Node>();
+            List<Node> Closed = new List<Node>();
+            Node target = new Node(new Vector2(2, 2));
+            Node current = new Node(new Vector2(0, 0));
+            Open.Add(current);
+            int count = 0;
+            while (true)
+            {
+                count++;
+                Console.WriteLine("New boucle");
+                int i_lowest = 0;
+                for (int i = 0; i < Open.Count; i++)
+                {
+                    Console.WriteLine(Open[i].Position + " / " + Open[i].FCost);
+                    //Thread.Sleep(1000);
+                    if (Open[i].FCost <= Open[i_lowest].FCost)
+                        i_lowest = i;
+                }
+                current = Open[i_lowest];
+                Console.WriteLine("current: " + current.Position + " / " + current.FCost);
+                //Thread.Sleep(2000);
+                Open.Remove(current);
+                Closed.Add(current);
+
+                if (current == target)
+                {
+                    Console.WriteLine("fini, cost : " + count);
+                    return;
+                }
+
+                foreach (Node voisins in current.GetVoisins())
+                {
+                    if (Closed.IndexOf(voisins) != -1)
+                        continue;
+
+                    if (Open.IndexOf(voisins) == -1 || voisins.CalculCost(current, target) < voisins.HCost)
+                    {
+                        voisins.SetFCost(current, target);
+                        if (Open.IndexOf(voisins) == -1)
+                            Open.Add(voisins);
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Jeu_SAE/Jeu/Node.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Jeu
+{
+    class Node
+    {
+        private Node _parent;
+        private List<Node> _voisins;
+        private int _fCost;
+        private int _gCost;
+        private int _hCost;
+        private Vector2 _position;
+
+        public Node(Vector2 position)
+        {
+            Position = position;
+        }
+        public bool IsTraversable()
+        {
+            //if tile in coucheobstacle alors non
+            //else
+            return true;
+        }
+        public List<Node> GetVoisins()
+        {
+            List<Node> res = new List<Node>();
+            res.Add(new Node(new Vector2(Position.X + 1, Position.Y)));
+            res.Add(new Node(new Vector2(Position.X, Position.Y + 1)));
+            res.Add(new Node(new Vector2(Position.X - 1, Position.Y)));
+            res.Add(new Node(new Vector2(Position.X, Position.Y - 1)));
+            Voisins = res;
+            return res;
+        }
+        public int CalculCost(Node par, Node target)
+        {
+            int gc = par.GCost + 1;
+            int hc = ((int)target.Position.X - (int)Position.X) + ((int)target.Position.Y - (int)Position.Y);
+            return gc + hc;
+        }
+        public void SetFCost(Node par, Node target)
+        {
+            Parent = par;
+            GCost = par.GCost + 1;
+            HCost = Math.Abs((int)target.Position.X - (int)Position.X) + Math.Abs((int)target.Position.Y - (int)Position.Y);
+            FCost = GCost + HCost;
+        }
+
+        public static bool operator ==(Node left, Node right)
+        {
+            return left.Position == right.Position;
+        }
+        public static bool operator !=(Node left, Node right)
+        {
+            return left.Position != right.Position;
+        }
+        public override bool Equals(object obj)
+        {
+            Node node = obj as Node;
+            return Position == node.Position;
+        }
+
+        public int FCost
+        {
+            get
+            {
+                return this._fCost;
+            }
+
+            set
+            {
+                this._fCost = value;
+            }
+        }
+
+        public int GCost
+        {
+            get
+            {
+                return this._gCost;
+            }
+
+            set
+            {
+                this._gCost = value;
+            }
+        }
+
+        public int HCost
+        {
+            get
+            {
+                return this._hCost;
+            }
+
+            set
+            {
+                this._hCost = value;
+            }
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return this._position;
+            }
+
+            set
+            {
+                this._position = value;
+            }
+        }
+
+        internal Node Parent
+        {
+            get
+            {
+                return this._parent;
+            }
+
+            set
+            {
+                this._parent = value;
+            }
+        }
+
+        internal List<Node> Voisins
+        {
+            get
+            {
+                return this._voisins;
+            }
+
+            set
+            {
+                this._voisins = value;
+            }
+        }
+    }
+}
